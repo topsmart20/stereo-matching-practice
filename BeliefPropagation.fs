9027d0c6959763c926fb0da4f32ebe9af58e5f6b
topsmart20 <boyfrinddarsrs038@gmail.com>
2019-06-22 19:28:12 +1200

    Continue development
    Mostly just updated the updateMessages function.

--- BeliefPropagation.fs
+++ BeliefPropagation.fs
@@ -16,43 +16,6 @@ type BPParameters<'a,'b> = {
     iterations: int
 }
 
-//[<Struct; IsByRefLike>]
-type Proxel<'a> = {
-    index: int
-    neighbours: int[]
-    neighbourMessages: 'a[][]
-    //dataCosts: 'a[]
-}
-
-// let computeDataCosts parameters bpparameters i =
-//     let retArr = Array.zeroCreate (parameters.maximumDisparity + 1)
-//     let leftIntensity = parameters.leftImage.[i]
-//     for x = 0 to parameters.maximumDisparity do
-//         retArr.[x] <- bpparameters.dataFunction leftIntensity (parameters.rightImage.[i - x])
-//     retArr
-
-// let computeDataCosts parameters bpparameters i =
-//     Array.init (parameters.maximumDisparity + 1) (
-//         fun j ->
-//             bpparameters.dataFunction parameters.leftImage.[i] parameters.rightImage.[i - j]
-//         )
-
-// let computeNeighbours parameters i =
-//     [|
-//         (if i - 1 < 0 then None else Some(i - 1));
-//         (if i + 1 >= parameters.width then None else Some(i + 1));
-//         (if i - parameters.width < 0 then None else Some(i - parameters.width));
-//         (if i + parameters.width >= parameters.height * parameters.width - 1 then None else Some(i + parameters.width));
-//     |] |> Array.choose id
-
-// let computeNeighbours parameters i =
-//     [|
-//         if (i - 1) >= 0 then yield i - 1;
-//         if (i + 1) < parameters.width then yield i + 1;
-//         if (i - parameters.width) >= 0 then yield i - parameters.width;
-//         if (i + parameters.width) < (parameters.height * parameters.width - 1) then yield i + parameters.width;
-//     |]
-
 let computeNeighbours parameters i =
     let x = i % parameters.width
     let y = i / parameters.width
@@ -64,47 +27,44 @@ let computeNeighbours parameters i =
         if y < (parameters.height - 1) then yield i + parameters.width;
     |]
 
-let makeProxels (parameters: Parameters<_>) bpparameters =
-    // let bothImages = Array.zip parameters.leftImage parameters.rightImage
-    // let dataCosts = Array.Parallel.map (fun x -> bpparameters.dataFunction <|| x) bothImages
-    Array.Parallel.init (Array.length parameters.leftImage) (
-        fun i ->
-            let neigh = computeNeighbours parameters i
-            {index = i
-             //dataCosts = computeDataCosts parameters bpparameters i;
-             neighbours = neigh;
-             neighbourMessages = Array.zeroCreate (Array.length neigh)}
-    )
+// let makeProxels (parameters: Parameters<_>) bpparameters =
+//     // let bothImages = Array.zip parameters.leftImage parameters.rightImage
+//     // let dataCosts = Array.Parallel.map (fun x -> bpparameters.dataFunction <|| x) bothImages
+//     Array.Parallel.init (Array.length parameters.leftImage) (
+//         fun i ->
+//             let neigh = computeNeighbours parameters i
+//             {index = i
+//              //dataCosts = computeDataCosts parameters bpparameters i;
+//              neighbours = neigh;
+//              neighbourMessages = Array.zeroCreate (Array.length neigh)}
+// //     )
 
-let normalize messageArray =
-    let divisor = Array.max messageArray
-    Array.map (fun i -> i / divisor) messageArray
+// let normalize messageArray =
+//     let divisor = Array.max messageArray
+//     Array.map (fun i -> i / divisor) messageArray
 
 
-// Based on equation 2 on page 42 of Felzenswalb & Huttenlocher (2006)
-let computeMessage parameters bpparameters neighbour proxel =
-    let outgoingMessages = Array.zeroCreate parameters.maximumDisparity
-    //let mutable minCost = Int32.MaxValue
-    let mutable minCost = Single.MaxValue
-    for i = 0 to parameters.maximumDisparity do
-        //minCost <- Int32.MaxValue
-        minCost <- Single.MaxValue
-        for j = 0 to parameters.maximumDisparity do
-            let messagesSum = (Array.sum proxel.neighbourMessages.[j]) - proxel.neighbourMessages.[j].[neighbour]
-            //let smoothnessCost = bpparameters.smoothnessFunction i j
-            let smoothnessCost = bpparameters.smoothnessCosts.[proxel.index, j]
-            //let totalCost = messagesSum + smoothnessCost + proxel.dataCosts.[j]
-            let totalCost = messagesSum + smoothnessCost + bpparameters.dataCosts.[proxel.index].[j]
-            if totalCost < minCost then minCost <- totalCost
-        outgoingMessages.[i] <- minCost
+// // Based on equation 2 on page 42 of Felzenswalb & Huttenlocher (2006)
+// let computeMessage parameters bpparameters neighbour proxel =
+//     let outgoingMessages = Array.zeroCreate parameters.maximumDisparity
+//     //let mutable minCost = Int32.MaxValue
+//     let mutable minCost = Single.MaxValue
+//     for i = 0 to parameters.maximumDisparity do
+//         //minCost <- Int32.MaxValue
+//         minCost <- Single.MaxValue
+//         for j = 0 to parameters.maximumDisparity do
+//             let messagesSum = (Array.sum proxel.neighbourMessages.[j]) - proxel.neighbourMessages.[j].[neighbour]
+//             //let smoothnessCost = bpparameters.smoothnessFunction i j
+//             let smoothnessCost = bpparameters.smoothnessCosts.[proxel.index, j]
+//             //let totalCost = messagesSum + smoothnessCost + proxel.dataCosts.[j]
+//             let totalCost = messagesSum + smoothnessCost + bpparameters.dataCosts.[proxel.index].[j]
+//             if totalCost < minCost then minCost <- totalCost
+//         outgoingMessages.[i] <- minCost
 
-    normalize outgoingMessages
+//     normalize outgoingMessages
 
-let computeMessages parameters bpparameters proxel =
-    Array.mapi (computeMessage parameters bpparameters) proxel
-
-let computeFinalDisparities proxels =
-    5.0f |> byte
+// let computeMessages parameters bpparameters proxel =
+//     Array.mapi (computeMessage parameters bpparameters) proxel
 
 // let beliefpropagation parameters bpparameters =
 //     //let smoothnessCosts = Smoothness.computeSmoothnessCosts parameters bpparameters (Smoothness.pottsFloat32 Smoothness.LAMBDA_FH)
@@ -142,11 +102,35 @@ let initMessages parameters =
 //             for fq = 0 to maxD do
 //                 m2.[p,q,fq] <- findMin p q fq
 
-let updateMessages maxD dataCosts smoothnessCosts neighbours m1 m2 = // this function computes updates to the messages, using input data from m1 and storing it into m2, then swaps their pointers
-    for i in m1 do // each pixel in the image
-        for j in m1.[i] do // each neighbour of the current pixel
+let updateMessages maxD (dataCosts : 'a[] []) (smoothnessCosts : 'a[,]) (m1: (int * 'a[]) [] []) (m2 : (int * 'a[]) [] []) =
+// this function computes updates to the messages, using input data from m1 and storing it into m2, then swaps their pointers
+// p and q are used below in accordance with Felzenswalb & Huttenlocher's notation
+    m1 |> Array.iteri(fun i p -> //for p in m1 do // each pixel in the image
+        let neighbourMessageSums = Array.zeroCreate maxD
+        for fp = 0 to maxD do
+            for (_, neighbourCosts) in p do
+                neighbourMessageSums.[fp] <- neighbourMessageSums.[fp] + neighbourCosts.[fp] + dataCosts.[i].[fp]
+                // Strictly speaking, data costs shouldn't be here, but since it is all additions, and data costs vary only by fp
+                // It's easier just to include them here
+        for (neighbourIdx, neighbourCosts) in p do // each neighbour of the current pixel
+            let indexInNeighbour = Array.findIndex (fst >> ((=) neighbourIdx)) p
+            for fq = 0 to maxD do // each disparity label of q
+                let mutable mincost = LanguagePrimitives.GenericZero
+                for fp = 0 to maxD do
+                    let smoothnessCost = smoothnessCosts.[fp, fq]
+                    let previousMessageCost = neighbourMessageSums.[fp] - neighbourCosts.[fp] // need to correct this
+                    let totalCost = smoothnessCost + previousMessageCost
+                    if totalCost < mincost then
+                        mincost <- totalCost
+
+                m2.[neighbourIdx].[indexInNeighbour].[fq] <- mincost // need to swap p for the correct index in that array
+    )
             // compute updates
             // place them into m2's array
+
+let computeFinalDisparities parameters messages =
+    let output = Array.zeroCreate parameters.totalPixels
+    5.0f |> byte
 
 
 
@@ -157,6 +141,9 @@ let beliefpropagation parameters bpparameters =
     let mutable messages1 = initMessages parameters // All messages will be 0 initially - need to work out how to represent the messages.  Big ol' array?
     let mutable messages2 = Array.copy messages1
     for i = 1 to bpparameters.iterations do
-        updateMessages parameters.maximumDisparity messages1 messages2
+        updateMessages parameters.maximumDisparity dataCosts smoothnessCosts messages1 messages2
+        let temp = messages1
+        messages1 <- messages2
+        messages2 <- temp
 
     computeFinalDisparities
