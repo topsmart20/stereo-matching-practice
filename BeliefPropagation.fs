30a2765717a08d6ceab89c1d9c8e08fa7da13cd3
topsmart20 <boyfrinddarsrs038@gmail.com>
2019-07-31 21:35:03 +1200

    Introduce a Parallel for loop

--- BeliefPropagation.fs
+++ BeliefPropagation.fs
@@ -13,7 +13,6 @@ type BPParameters = {
     iterations: int
 }
 
-
 let computeNeighbours parameters i =
     let x = i % parameters.width
     let y = i / parameters.width
@@ -51,7 +50,7 @@ let dtFH m =
 
 // This is intended to match eq. 2 in F & H 2006
 //let updateMessages maxD (dataCosts : float32 [][]) (smoothnessCosts : float32 [,]) (m1 : (int * float32 []) [] []) (m2 : (int * float32 []) [] []) =
-let updateMessages maxD (dataCosts : float32 [][]) (smoothnessCosts : float32 [,]) oddOrEven (m : (int * float32 []) [] []) =
+let updateMessages maxD (dataCosts : float32 [][]) (smoothnessCosts : float32 [,]) indexIsEven (m : (int * float32 []) [] []) =
 // p and q are used below in accordance with Felzenswalb & Huttenlocher's notation
     // //Array.Parallel.iteri (fun i p -> // each pixel in the image
     // //Array.iteri (fun i p -> // each pixel in the image
@@ -87,18 +86,40 @@ let updateMessages maxD (dataCosts : float32 [][]) (smoothnessCosts : float32 [,
     // D_p(f_p) + the sum of the messages for f_p received from the other neighbours at t-1
     //Array.Parallel.iteri (fun i p -> // each pixel in the image
     //Array.iteri (fun i p -> // each pixel in the image
-    let startIndex = if oddOrEven then 0 else 1
-    let neighbourMessageSums = Array.zeroCreate (maxD + 1)
-    for i in startIndex..2..((Array.length m) - 1) do
+    // let startIndex = if oddOrEven then 0 else 1
+    // let neighbourMessageSums = Array.zeroCreate (maxD + 1)
+    // for i in startIndex..2..((Array.length m) - 1) do
+    //     System.Array.Fill(neighbourMessageSums, 0.0f)
+    //     let p = m.[i]
+    //     for fp = 0 to maxD do
+    //         for (_, (neighbourCosts : float32 [])) in p do
+    //             neighbourMessageSums.[fp] <- neighbourMessageSums.[fp] + neighbourCosts.[fp] + dataCosts.[i].[fp]
+
+    //     for ((neighbourIdx : int), (neighbourCosts : float32 [])) in p do // each neighbour of the current pixel
+    //         let hfp = Array.map2 (( - )) neighbourMessageSums neighbourCosts
+    //         let minhfp = (Array.min hfp) + Smoothness.D_FH
+    //         dtFH hfp
+    //         truncateFH minhfp hfp
+    //         let indexInNeighbour = Array.findIndex (fst >> ((=) i)) m.[neighbourIdx]
+    //         let (_, mneighbourcosts) = m.[neighbourIdx].[indexInNeighbour]
+    //         for fq = 0 to maxD do // each disparity label of q
+    //             mneighbourcosts.[fq] <- min hfp.[fq] minhfp
+
+    let startIndex = if indexIsEven then 0 else 1
+    // let neighbourMessageSums = Array.zeroCreate (maxD + 1)
+    let processPixel loopIndex _loop neighbourMessageSums =
+        //fun loopIndex _loop neighbourMessageSums ->
         System.Array.Fill(neighbourMessageSums, 0.0f)
+        let i =
+            if indexIsEven then
+                loopIndex * 2
+            else
+                loopIndex * 2 - 1
         let p = m.[i]
-        //let fpMax = min maxD ((Array.length dataCosts.[i]) - 1)
-        // let neighbourMessageSums = Array.zeroCreate (fpMax + 1)
         for fp = 0 to maxD do
             for (_, (neighbourCosts : float32 [])) in p do
                 neighbourMessageSums.[fp] <- neighbourMessageSums.[fp] + neighbourCosts.[fp] + dataCosts.[i].[fp]
-                // Strictly speaking, data costs shouldn't be here, but since it is all additions, and data costs vary only by fp
-                // It's easier just to include them here
+
         for ((neighbourIdx : int), (neighbourCosts : float32 [])) in p do // each neighbour of the current pixel
             let hfp = Array.map2 (( - )) neighbourMessageSums neighbourCosts
             let minhfp = (Array.min hfp) + Smoothness.D_FH
@@ -108,6 +129,35 @@ let updateMessages maxD (dataCosts : float32 [][]) (smoothnessCosts : float32 [,
             let (_, mneighbourcosts) = m.[neighbourIdx].[indexInNeighbour]
             for fq = 0 to maxD do // each disparity label of q
                 mneighbourcosts.[fq] <- min hfp.[fq] minhfp
+
+        neighbourMessageSums
+
+    let createThreadLocalArray = fun () -> Array.zeroCreate (maxD + 1)
+
+    Threading.Tasks.Parallel.For(
+        startIndex,
+        ((Array.length m) / 2),
+        createThreadLocalArray,
+        processPixel,
+        ignore
+    ) |> ignore
+
+    // for i in startIndex..2..((Array.length m) - 1) do
+    //     System.Array.Fill(neighbourMessageSums, 0.0f)
+    //     let p = m.[i]
+    //     for fp = 0 to maxD do
+    //         for (_, (neighbourCosts : float32 [])) in p do
+    //             neighbourMessageSums.[fp] <- neighbourMessageSums.[fp] + neighbourCosts.[fp] + dataCosts.[i].[fp]
+
+    //     for ((neighbourIdx : int), (neighbourCosts : float32 [])) in p do // each neighbour of the current pixel
+    //         let hfp = Array.map2 (( - )) neighbourMessageSums neighbourCosts
+    //         let minhfp = (Array.min hfp) + Smoothness.D_FH
+    //         dtFH hfp
+    //         truncateFH minhfp hfp
+    //         let indexInNeighbour = Array.findIndex (fst >> ((=) i)) m.[neighbourIdx]
+    //         let (_, mneighbourcosts) = m.[neighbourIdx].[indexInNeighbour]
+    //         for fq = 0 to maxD do // each disparity label of q
+    //             mneighbourcosts.[fq] <- min hfp.[fq] minhfp
 
     //Array.Parallel.iter (fun p ->
     Array.iter (fun p ->
@@ -136,7 +186,7 @@ let beliefpropagation parameters bpparameters =
     let dataCosts = Data.computeDataCosts parameters bpparameters.dataFunction
     let smoothnessCosts = Smoothness.computeSmoothnessCosts parameters bpparameters.smoothnessFunction
     let messages = initMessages parameters dataCosts
-    let mutable indexIsEven = false
+    let mutable indexIsEven = true // start with 0..2..etc
     for _i = 1 to bpparameters.iterations do
         updateMessages parameters.maximumDisparity dataCosts smoothnessCosts indexIsEven messages
         indexIsEven <- not indexIsEven
